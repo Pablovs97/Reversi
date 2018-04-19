babff3265484bf5e399f7fc0d0da2748b6a4a9ca
Pablo J. Vilas <pvs5@alumnes.udl.cat>
2018-04-19 15:37:44 +0200

    First commit

---
+++ app/src/main/java/com/example/pablovilas/reversi/Menu_Principal.java
@@ -0,0 +1,13 @@
+package com.example.pablovilas.reversi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu_Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__principal);
+    }
+}
